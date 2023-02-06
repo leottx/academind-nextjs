abd570cbe5132a6bc0aa1d66a8624898d3570251
leotx <tteixeira.leonardo@gmail.com>
2023-02-06 16:34:44 -0300

    feat: nextjs link component and not-found page

--- 03-module/pages/clients/index.js
+++ 03-module/pages/clients/index.js
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 export default function Clients() {
+  const clients = [
+    {
+      id: 1,
+      name: "John Doe",
+    },
+    {
+      id: 2,
+      name: "Leonardo Teixeira",
+    },
+    {
+      id: 3,
+      name: "Manuel",
+    },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      {clients.map((client) => (
+        <li key={client.id}>
+          <Link href={`clients/${client.id}`}>{client.name}</Link>
+        </li>
+      ))}
     </div>
   );
 }
